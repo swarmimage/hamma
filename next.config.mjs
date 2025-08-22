14efea6b8fd6193a8fa4e2d1785fa1b2d1710335
Sardor Akbarov <swarmimage@gmail.com>
2025-08-22 18:32:10 +0500

    fix: pages in src for Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  webpack(config) {
+    // Указываем Next.js, что папка с страницами находится в src/pages
+    config.resolve.modules.push(path.resolve('./src'));
+    return config;
+  },
 };
 
 export default nextConfig;
+  
